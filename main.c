70b72a047faa34a256a712dbb821c46e6c75aa2d
Douglas S. Elias <douglassantoselias@gmail.com>
2023-11-08 15:04:20 -0300

    rename variables

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main() {
   InitWindow(screen_width, screen_height, "Game of Life");
   SetTargetFPS(10);
 
-  const int grid_width = (screen_width / 10);
-  const int grid_height = (screen_height / 10);
+  const int grid_width = screen_width / 10;
+  const int grid_height = screen_height / 10;
   bool grid[grid_width][grid_height] = {};
   for (int i = 0; i < grid_width; i++) {
     for (int j = 0; j < grid_height; j++) {
@@ -15,46 +15,45 @@ int main() {
     }
   }
 
-  int cell_size = 10;
+  const int cell_size = 10;
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
 
     for (int i = 0; i < grid_width; i++) {
       for (int j = 0; j < grid_height; j++) {
-        if (grid[i][j])
+        bool is_cell_alive = grid[i][j];
+        if (is_cell_alive)
           DrawRectangle(i * cell_size, j * cell_size, cell_size, cell_size,
                         GREEN);
 
-        int sum = 0;
+        int total_neighbors_alive = 0;
         if (grid[i - 1][j])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i + 1][j])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i][j + 1])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i][j - 1])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i - 1][j - 1])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i - 1][j + 1])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i + 1][j - 1])
-          sum += 1;
+          total_neighbors_alive += 1;
         if (grid[i + 1][j + 1])
-          sum += 1;
+          total_neighbors_alive += 1;
 
-        if (grid[i][j]) {
-          // cell is alive
-          if (sum < 2)
+        if (is_cell_alive) {
+          if (total_neighbors_alive < 2)
             grid[i][j] = false;
-          else if (sum < 4)
+          else if (total_neighbors_alive < 4)
             grid[i][j] = true;
-          else if (sum < 9)
+          else if (total_neighbors_alive < 9)
             grid[i][j] = false;
         } else {
-          // cell is dead
-          if (sum == 3)
+          if (total_neighbors_alive == 3)
             grid[i][j] = true;
           else
             grid[i][j] = false;
